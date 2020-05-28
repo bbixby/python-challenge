cc5c47ae937dcc2e6f74eed6d5e1f0794b3a069b
Matthew Debnar <matthew.debnar@gmail.com>
2020-05-28 14:49:14 -0400

    PyPoll working results; needs print format and output txt

--- PyPoll/PyPollWIP.py
+++ PyPoll/PyPollWIP.py
@@ -4,20 +4,49 @@ import csv
 #set local pc path to data csv file
 election_csv = os.path.join("..", "..", "election_data.csv")
 #create votes dictionary
-votes = {"candidate", "voteCount"}
-votes = dict()
+votes = {}
+#votes = dict()
 #open and read csv
 with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #skip the header row
     csv_header = next(csvfile)
+    #run through the file
     for row in csvreader:
+        #define candidate column
         candidate = row[2]
+        #if candidate not in dictionary, add with first vote
         if candidate not in votes:
-            votes["candidate"] = candidate
-            votes["voteCount"] = 1
+            votes[candidate] = 1
+        #if candidate already in dictionary, add a vote
         else:
-            votes["candidate"] = candidate
-            votes["voteCount"] = votes["voteCount"] + 1
+            votes[candidate] += 1
+#check the results
+#for key, value in votes.items():
+    #print(f'{key} : {value}')
+
+#add the dictionary keys for vote total
+values = votes.values()
+voteTotal = sum(values)
+#print (voteTotal)
+
+#create the winners, percent, and votes per as a list
+#lst=[]
+#for key, value in votes.items():
+    #lst.append(f"{key}: {value / voteTotal:.3%} ({value})")
+
+#output to terminal and to text file
+#output = (
+print(f"Election Results\n")
+print(f"-------------------------\n")
+print(f"Total Votes: {voteTotal}\n")
+print(f"-------------------------\n")
+    #f"{lst}"
 for key, value in votes.items():
-    print(f'{key} : {value}')
+    print(f"{key}: {value / voteTotal:.3%} ({value})")
+print(f"-------------------------\n")
+print(f"Winner: {max(votes, key=votes.get)}\n")
+print(f"-------------------------")
+    #)
+#print(output)
+
